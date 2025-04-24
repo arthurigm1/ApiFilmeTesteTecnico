17ae4a03044adc0d371f285e3144a1c68b2fb211
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-04-24 10:47:52 -0300

    feat: implementar serviço de filmes com operações CRUD

---
+++ src/service/filmeService.ts
@@ -0,0 +1,45 @@
+import { FilmeModel } from "../models/filmeModel";
+
+const filmemodel = FilmeModel;
+export const FilmeService = {
+  async create(data: {
+    titulo: string;
+    genero: string;
+    duracao: number;
+    data_lancamento: Date;
+  }) {
+    return await filmemodel.create(data);
+  },
+
+  async delete(id: string) {
+    const filme = await filmemodel.findbyId(id);
+    if (filme !== null) {
+      return await filmemodel.delete(id);
+    } else {
+    }
+  },
+
+  async findbyId(id: string) {
+    return await filmemodel.findbyId(id);
+  },
+
+  async update(
+    id: string,
+    dados: {
+      titulo?: string; //update em qualquer dado ?
+      genero?: string;
+      duracao?: number;
+      data_lancamento?: Date;
+    }
+  ) {
+    return await filmemodel.update(id, dados);
+  },
+
+  async findAll() {
+    return await filmemodel.findAll();
+  },
+
+  async findbyTitulo(titulo: string) {
+    return await filmemodel.findbyTitulo(titulo);
+  },
+};
